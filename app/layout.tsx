c8baffcbd17134a4dd16288f5f5cc6a3be7219d1
Joaquin Metayer <joaquinmetayer@gmail.com>
2024-04-22 04:19:11 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  const header = (
+    <header>
+      <Link href="/">
+        <h1 className="text-xl font-bold py-6">Joaquin Metayer</h1>
+      </Link>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <div className="py-6">
+      </div>
+    </footer>
+  );
+
+  return (
+    <html>
+      <head />
+      <body>
+        <div className="mx-auto max-w-2xl px-8 py-2">
+          {header}
+          {children}
+          {footer}
+        </div>
+      </body>
+    </html>
+  );
+}
